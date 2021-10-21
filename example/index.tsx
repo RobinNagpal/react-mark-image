75fc33c7e692fadfd20697ac654eeaaff762c80b
robin <robinnagpal.tiet@gmail.com>
2021-10-21 12:18:40 -0400

    Update the example

--- example/index.tsx
+++ example/index.tsx
@@ -1,12 +1,44 @@
 import 'react-app-polyfill/ie11';
+import { useState } from 'react';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Thing } from '../.';
+import { Annotation, IAnnotation } from '../.';
 
 const App = () => {
+  const [annotations, setAnnotations] = useState<any[]>([]);
+
+  const [annotation, setAnnotation] = useState<any>({});
+
+  const onSubmit = async (annotation: IAnnotation) => {
+    const { geometry, data } = annotation;
+
+    setAnnotation({});
+
+    const updatedAnnotations = [
+      ...annotations,
+      {
+        geometry,
+        data: {
+          ...data,
+          id: Math.random(),
+        },
+      },
+    ];
+    setAnnotations(updatedAnnotations);
+  };
+
   return (
-    <div>
-      <Thing />
+    <div style={{ width: '1000px', height: '700px' }}>
+      <Annotation
+        src={
+          'https://cdn.pixabay.com/photo/2015/09/04/18/55/yoda-922520_960_720.png'
+        }
+        alt="Two pebbles anthropomorphized holding hands"
+        annotations={annotations}
+        value={annotation}
+        onChange={setAnnotation}
+        onSubmit={onSubmit}
+      />
     </div>
   );
 };
