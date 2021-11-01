23f802eec658db78fbfd2d7a81a08964c02673bf
robin <robinnagpal.tiet@gmail.com>
2021-11-01 15:29:34 -0400

    Make the typescript paths work

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,13 @@
 module.exports = {
+  // Helps resolve paths - https://github.com/storybookjs/storybook/issues/3291#issuecomment-609468718
+  webpackFinal: (config) => {
+    config.resolve.modules.push(process.cwd() + '/node_modules');
+    config.resolve.modules.push(process.cwd() + '/src');
+
+    // this is needed for working w/ linked folders
+    config.resolve.symlinks = false;
+    return config;
+  },
   stories: ['../src/**/*.stories.@(ts|tsx|js|jsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   // https://storybook.js.org/docs/react/configure/typescript#mainjs-configuration
