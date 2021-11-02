9c2120d84f99dd52c9e314a34226335de6e71c87
robin <robinnagpal.tiet@gmail.com>
2021-11-02 14:44:26 -0400

    export shapes

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,7 @@ export { default as Annotation } from './components/Annotation';
 export { default as RectangleSelector } from './hocs/RectangleSelector';
 export { default as PointSelector } from './hocs/PointSelector';
 export { default as OvalSelector } from './hocs/OvalSelector';
+export { default as Oval } from './components/Shapes/Oval';
+export { default as Point } from './components/Shapes/Point';
+export { default as Rectangle } from './components/Shapes/Rectangle';
 export * from './types';
